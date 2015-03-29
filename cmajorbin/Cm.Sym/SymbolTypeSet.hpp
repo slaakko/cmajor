bb4caff77b003addef7edacb5535d2e7007508e9
slaakko <slaakko@c2eb7466-7d64-40cb-83a7-a78a24d08109>
2015-03-29 19:11:33 +0000

    name resolution 2

---
+++ cmajorbin/Cm.Sym/SymbolTypeSet.hpp
@@ -0,0 +1,74 @@
+/*========================================================================
+    Copyright (c) 2012-2015 Seppo Laakko
+    http://sourceforge.net/projects/cmajor/
+
+    Distributed under the GNU General Public License, version 3 (GPLv3).
+    (See accompanying LICENSE.txt or http://www.gnu.org/licenses/gpl.html)
+
+========================================================================*/
+
+#ifndef CM_SYM_SYMBOL_TYPE_SET_INCLUDED
+#define CM_SYM_SYMBOL_TYPE_SET_INCLUDED
+#include <Cm.Sym/Symbol.hpp>
+#include <stack>
+#include <unordered_set>
+
+namespace Cm { namespace Sym {
+
+enum class SymbolTypeSetId : uint8_t
+{
+    lookupAllSymbols,
+    lookupTypeSymbols,
+    lookupTypeAndFunctionGroupSymbols,
+    lookupDotSubjectSymbols,
+    lookupContainerSymbols,
+    lookupClassAndNamespaceSymbols,
+    lookupClassSymbols,
+    lookupFunctionGroupAndMemberVariable,
+    lookupLocalVariable,
+    lookupMemberVariable,
+    lookupParameter,
+    lookupTypedef,
+    lookupVariableAndParameter,
+    lookupInvokeSubject,
+    lookupSizeOfSubject,
+    lookupCastSource,
+    lookupArgumentSymbol,
+    lookupTypenameSubject,
+    lookupFunctionGroup,
+    lookupNamespace,
+    lookupTypeAndConceptSymbols,
+    lookupConceptGroup,
+    lookupConstant,
+    lookupDelegate,
+    lookupClassDelegate,
+    lookupEnumType,
+    lookupEnumConstant,
+    lookupConstantAndEnumConstantSymbols,
+    max 
+};
+
+class LookupIdStack
+{
+public:
+    void Push(SymbolTypeSetId id) { stack.push(id); }
+    SymbolTypeSetId Pop() { SymbolTypeSetId id = stack.top(); stack.pop(); return id; }
+private:
+    std::stack<SymbolTypeSetId> stack;
+};
+
+class SymbolTypeSetCollection
+{
+public:
+    SymbolTypeSetCollection();
+    SymbolTypeSet& GetSymbolTypeSet(SymbolTypeSetId id);
+private:
+    std::vector<SymbolTypeSet> symbolTypeSets;
+};
+
+SymbolTypeSetCollection* GetSymbolTypeSetCollection();
+void SetSymbolTypeSetCollection(SymbolTypeSetCollection* collection);
+
+} } // namespace Cm::Sym
+
+#endif // CM_SYM_SYMBOL_TYPE_SET_INCLUDED
