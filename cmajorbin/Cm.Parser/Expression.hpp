f7c16df59d4fbe35e3dcd4228b8fabd78892e237
slaakko <slaakko@c2eb7466-7d64-40cb-83a7-a78a24d08109>
2015-08-17 20:35:26 +0000

    cmcm 7

--- cmajorbin/Cm.Parser/Expression.hpp
+++ cmajorbin/Cm.Parser/Expression.hpp
@@ -1,5 +1,5 @@
-#ifndef Expression_hpp_24386
-#define Expression_hpp_24386
+#ifndef Expression_hpp_6309
+#define Expression_hpp_6309
 
 #include <Cm.Parsing/Grammar.hpp>
 #include <Cm.Parsing/Keyword.hpp>
@@ -44,4 +44,4 @@ private:
 
 } } // namespace Cm.Parser
 
-#endif // Expression_hpp_24386
+#endif // Expression_hpp_6309
