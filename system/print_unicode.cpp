826f768e49eeb331904d321dc4baa3e48152f859
slaakko <slaakko@c2eb7466-7d64-40cb-83a7-a78a24d08109>
2016-03-04 17:51:58 +0000

    unicode 3

---
+++ system/print_unicode.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+extern "C" void print_unicode_string(int handle, wchar_t* ws)
+{
+#ifdef _WIN32
+    if (handle == 1)
+    {
+        std::wcout.flush();
+    }
+    else if (handle == 2)
+    {
+        std::wcerr.flush();
+    }
+    int prevMode = _setmode(handle, _O_U16TEXT);
+#endif    
+   if (handle == 1)
+   {
+       std::wcout << ws;
+   }
+   else if (handle == 2)
+   {
+       std::wcerr << ws;
+   }
+#ifdef _WIN32
+    if (handle == 1)
+    {
+        std::wcout.flush();
+    }
+    else if (handle == 2)
+    {
+        std::wcerr.flush();
+    }
+    _setmode(handle, prevMode);
+#endif    
+}
